1209a72a881567d03f36d30a830924012703e4fb
Almira_Maharani <almira.eka@ui.ac.id>
2022-12-03 08:23:33 +0700

    fix debug be delete content

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, add_konten, delete_konten
+from .views import index, add_konten, delete_konten, indexAturKonten
 
 urlpatterns = [
     path('',index,name='indexKonten'),
-    path('add',add_konten,name='addKonten'),
-    path('delete/<int:idKonten>',delete_konten, name='deleteKonten')
+    path('aturKonten/add',add_konten,name='addKonten'),
+    path('delete/<int:idKonten>',delete_konten, name='deleteKonten'),
+    path('aturKonten',indexAturKonten,name='aturKonten')
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from .models import Konten
 from .forms import KontenForm
-from django.http.response import HttpResponse, HttpResponseRedirect
+from django.http.response import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-import json
 from django.urls import reverse
 
 
 def index(request):
     posts = Konten.objects.all()
     response = {'posts':posts}
-    return render(request,'konten_list.html',response)
+    return render(request,'landing_page.html',response)
+
+def indexAturKonten(request):
+    posts = Konten.objects.all()
+    response = {'posts':posts}
+    return render(request,'atur_konten.html',response)
 
 @login_required(login_url='/hello')
 def add_konten(request):
     form = KontenForm(request.POST or None)
     if (form.is_valid() and request.method=='POST'):
         form.save()
-        return HttpResponseRedirect('')
+        return HttpResponseRedirect(reverse('aturKonten'))
     
     response = {'form':form}
     return render(request,'konten_form.html',response)
@@ -27,4 +30,4 @@ def add_konten(request):
 def delete_konten(request, idKonten):
     data = Konten.objects.get(idKonten=idKonten)
     data.delete()
-    return HttpResponseRedirect(reverse('konten_list.html'))
+    return HttpResponseRedirect(reverse('aturKonten'))
